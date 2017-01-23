299020ca034e8269e083cbb2cd34828accd3d02f
xuopled <xuopled@gmail.com>
2017-01-23 18:09:42 +0100

    Add target blank on external links

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,9 +1,28 @@
 import flatten, {unflatten} from "flat"
 import remark from "remark"
 import html from "remark-html"
+import select from "unist-util-select"
+
+const isRelativeUrlRegex = /^[^\/]+\/[^\/].*$|^\/[^\/].*$/
+
+function externalLink() {
+  return function(ast) {
+    select(ast, "link").forEach(function(node) {
+      if (!isRelativeUrlRegex.test(node.url)) {
+        node.data = {
+          hProperties: {
+            target: "_blank",
+            rel: "nofollow noopener noreferrer"
+          },
+        }
+      }
+    })
+  }
+}
 
 export function mdifyText(text) {
   return remark()
+    .use(externalLink)
     .use(html, {entities: "escape"})
     .process(text, {
       commonmark: true,
