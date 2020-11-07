7724a07c8d4f00ac85f716abf54d33a3b98eac6d
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-11-07 08:39:51 +0100

    Revert "Add Covid message"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,106 +1,85 @@
-import Image from "components/Image";
-import Nav from "components/Nav";
-import TextLink from "components/TextLink";
-import TopBar from "components/TopBar";
-import TopLogo from "components/TopLogo";
-import enhanceCollection from "phenomic/lib/enhance-collection";
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import { Icon } from "react-fa";
-import { injectIntl, intlShape } from "react-intl";
-import { connect } from "react-redux";
-import { Sticky } from "react-sticky";
-import { customFilter } from "utils/collection";
-import styles from "./index.css";
+import PropTypes from "prop-types"
+import React, {Component} from "react"
+import {connect} from "react-redux"
+import {Sticky} from "react-sticky"
+import {injectIntl, intlShape} from "react-intl"
+import enhanceCollection from "phenomic/lib/enhance-collection"
+
+import {customFilter} from "utils/collection"
+import Nav from "components/Nav"
+import TopBar from "components/TopBar"
+import TopLogo from "components/TopLogo"
+import Image from "components/Image"
+import TextLink from "components/TextLink"
+
+import styles from "./index.css"
 
 class Header extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       isNavOpen: false,
-    };
+    }
 
-    this.handleOpenNav = this.handleOpenNav.bind(this);
-    this.handleCloseNav = this.handleCloseNav.bind(this);
+    this.handleOpenNav = this.handleOpenNav.bind(this)
+    this.handleCloseNav = this.handleCloseNav.bind(this)
   }
 
   handleOpenNav() {
-    this.setState({ isNavOpen: true });
+    this.setState({isNavOpen: true})
   }
 
   handleCloseNav() {
-    this.setState({ isNavOpen: false });
+    this.setState({isNavOpen: false})
   }
 
   render() {
-    const { collection } = this.context;
-    const {
-      browser,
-      cover,
-      intl,
-      layoutToLink,
-      slogan,
-      text,
-      title,
-    } = this.props;
+    const {collection} = this.context
+    const {browser, cover, intl, layoutToLink, slogan, text, title} = this.props
 
     const PageToLink = enhanceCollection(collection, {
       filter: (page) => customFilter(page, intl.locale, layoutToLink),
-    }).shift();
+    }).shift()
 
     return (
       <header className={styles.header}>
-        <TopBar onOpenNav={this.handleOpenNav} slogan={slogan} />
+        <TopBar
+          onOpenNav={this.handleOpenNav}
+          slogan={slogan}
+        />
         <Sticky className={styles.logo} isActive={browser.greaterThan.m}>
           <TopLogo />
         </Sticky>
-        {this.state.isNavOpen && (
+        {this.state.isNavOpen &&
           <div className={styles.navOverlay} onClick={this.handleCloseNav} />
-        )}
+        }
         <Sticky isActive={browser.greaterThan.m}>
-          <Nav open={this.state.isNavOpen} />
+          <Nav
+            open={this.state.isNavOpen}
+          />
         </Sticky>
-        {cover && (
+        {cover &&
           <div className={styles.photo}>
-            <Image
-              src={cover.image}
-              alt={cover.alt}
-              sizes={["256", "512", "1024", "2048"]}
-            />
-            {title && <h1 className={styles.title}>{title}</h1>}
-            {text && PageToLink && (
+            <Image src={cover.image} alt={cover.alt} sizes={["256", "512", "1024", "2048"]} />
+            {title &&
+              <h1 className={styles.title}>{title}</h1>
+            }
+            {text && PageToLink &&
               <div className={styles.text}>
                 <TextLink text={text} url={PageToLink.__url} />
               </div>
-            )}
-            <span
-              style={{
-                position: "absolute",
-                bottom: "0",
-                width: "100%",
-                background: "#F44336",
-                color: "#fff",
-                padding: "10px 20px",
-              }}
-            >
-              <span style={{ marginRight: "10px" }}>
-                <Icon name="bell" />
-              </span>
-              {intl.locale === "nl"
-                ? "Info Covid-19: We zijn open vanaf 2 juni, met respect voor sanitaire maatregelen om u in de beste omstandigheden te ontvangen."
-                : "Info Covid-19 : Nous seront ouverts à partir du 2 juin en respectant les mesures sanitaires afin de vous recevoir dans les meilleures conditions."}
-            </span>
+            }
           </div>
-        )}
+        }
       </header>
-    );
+    )
   }
 }
 
 Header.contextTypes = {
   collection: PropTypes.array.isRequired,
-};
+}
 
 Header.propTypes = {
   browser: PropTypes.object.isRequired,
@@ -113,6 +92,8 @@ Header.propTypes = {
   slogan: PropTypes.string,
   text: PropTypes.string,
   title: PropTypes.string,
-};
+}
 
-export default connect(({ browser }) => ({ browser }))(injectIntl(Header));
+export default connect(
+  ({browser}) => ({browser}),
+)(injectIntl(Header))
