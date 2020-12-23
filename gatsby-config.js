c28c773c4c607f21f84a1abfd6d2f6f18cee35be
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-12-23 12:04:05 +0100

    open external link in new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
+          "gatsby-remark-external-links",
           {
             resolve: "gatsby-remark-images",
             options: {
